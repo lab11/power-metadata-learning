1c8bb0b7166b054ea8a25cc6110b197faae25ac5
Neal Jackson <nealjack@umich.edu>
2017-04-30 19:14:57 -0700

    some basic logic for choosing unseen device split

--- data_processing/generate_training.py
+++ data_processing/generate_training.py
@@ -13,6 +13,7 @@ from datetime import datetime, date
 import subprocess
 import glob
 
+unseenTestRatio = .2
 
 parser = argparse.ArgumentParser(description='Process data input files')
 parser.add_argument('inputdir', metavar='I', type=str, nargs='+',
@@ -27,21 +28,65 @@ args = parser.parse_args()
 labelFile = open(args.labelFile[0],'r')
 labels = labelFile.readline().strip().split(',')
 
-dataFileDict = {}
+labelToFilenames = {}
 for label in labels:
-    dataFileDict[label] = []
+    labelToFilenames[label] = []
 
 #now get a list of data files
 dataFiles = os.listdir(args.inputdir[0])
 
 for dataFile in dataFiles:
     label = dataFile.split('_')[1].split('.')[0]
-    if(label in dataFileDict):
-        dataFileDict[label].append(dataFile)
+    if(label in labelToFilenames):
+        labelToFilenames[label].append(dataFile)
     else:
         print("Warning: No label for {}".format(dataFile))
 
 print("Found following data files:")
 
-for key in dataFileDict:
-    print("{}: {} files".format(key,len(dataFileDict[key])))
+for key in labelToFilenames:
+    numFiles = len(labelToFilenames[key])
+    print("{}: {} files".format(key,numFiles))
+
+
+print('\nGenerate unseen set')
+
+labelToUnseen = {}
+labelToTest = {}
+for key in labelToFilenames:
+    numFiles = len(labelToFilenames[key])
+    if(numFiles <= 2):
+        print('No files for label ' + key + ', skipping...')
+        continue
+    numPoints = 0
+
+    # load devices
+    devices = []
+    for filename in labelToFilenames[key]:
+        device = np.load(args.inputdir[0] + '/' + filename)
+        devices.append(device)
+        numPoints += device.shape[0]
+    numTries = 0
+    while numTries < 10:
+        pick = np.random.choice(numFiles, int(np.ceil(numFiles * unseenTestRatio)), False)
+        unseenDevices = list(devices[i] for i in pick)
+        unseenNumPoints = 0
+        for device in unseenDevices:
+            unseenNumPoints += device.shape[0]
+        print(unseenNumPoints)
+        print(numPoints)
+        print()
+        if unseenNumPoints/numPoints > (unseenTestRatio - .1) and \
+            unseenNumPoints/numPoints < (unseenTestRatio + .1):
+            labelToUnseen[key] = unseenDevices
+            labelToTest[key] = devices
+            for ind in pick:
+                del labelToTest[key][ind]
+            break;
+        numTries += 1
+    if len(labelToUnseen[key]) == 0:
+        print('Failed to choose unseen device(s)')
+        exit()
+    #numTrain = int((1-unseenTestRatio)*numFiles)
+    #trainDict[key] = labelToFilenames[key][:numTrain]
+    #unseenTestDict[key] = labelToFilenames[key][numTrain:]
