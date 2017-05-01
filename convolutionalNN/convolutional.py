7a0a01d0c82a011e464496d9909132b8da1844de
Josh Adkins <adkinsjd@umich.edu>
2017-05-01 13:59:09 -0700

    added some more printing, fixed labels

--- convolutionalNN/convolutional.py
+++ convolutionalNN/convolutional.py
@@ -14,11 +14,11 @@ import numpy as np
 FLAGS = None
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial) 
+  return tf.Variable(initial)
 
 def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial) 
+  return tf.Variable(initial)
 
 def main(_):
 
@@ -37,13 +37,13 @@ def main(_):
   train_labels_validate = train_labels[int(len(train_data)*0.8):]
 
   import conv_config as config
-  
+
   #create an inverse logits ratio to scale the training to the number
   #of representations in the set
   bins, counts = np.unique(train_labels_train,return_counts=True)
   rep_sum = np.sum(counts)
   weight_vector = np.zeros(len(bins))
-  
+
   for i in range(0,len(counts)):
     weight_vector[i] = 1-(counts[i]/rep_sum)
 
@@ -54,7 +54,7 @@ def main(_):
 
   #generate two placeholder arrays for training and labels
   x = tf.placeholder(tf.float32, shape=[None, len(train_data[0])])
-  y_ = tf.placeholder(tf.float32, shape=[None,np.max(train_labels)+1])
+  y_ = tf.placeholder(tf.float32, shape=[None,1])
 
 
   x_data = tf.reshape(x,[-1,len(train_data[0]),1,1])
@@ -62,17 +62,17 @@ def main(_):
   #let's start by doing a pooling layer because this data is huge
   pre_pool = tf.nn.max_pool(x_data, ksize=[1,config.pre_pool_size,1,1], strides=[1,config.pre_pool_stride,1,1], padding='SAME')
 
- 
-  #do the first convolutional layer 
+
+  #do the first convolutional layer
   W_conv1 = weight_variable([config.conv1_filter_size,1,1,config.conv1_num_filters])
   b_conv1 = bias_variable([config.conv1_num_filters])
 
   conv1 = tf.nn.conv2d(pre_pool, W_conv1, strides=[1,1,1,1], padding='SAME')
   conv1_out = tf.nn.relu(conv1 + b_conv1)
-  
+
   #pool again every
   pool1 = tf.nn.max_pool(conv1_out, ksize=[1,config.pool1_size,1,1], strides=[1,config.pool1_stride,1,1], padding='SAME')
-  
+
   #layer two convolution 100x32 - 64filters
   W_conv2 = weight_variable([config.conv2_filter_size,1,config.conv1_num_filters,config.conv2_num_filters])
   b_conv2 = bias_variable([config.conv2_num_filters])
@@ -82,9 +82,9 @@ def main(_):
 
   #reduce by 4 just because it's devisible
   pool2 = tf.nn.max_pool(conv2_out, ksize=[1,config.pool2_size,1,1], strides=[1,config.pool2_stride,1,1], padding='SAME')
-  
+
   #data size should now be 1x216x64 filters
-  W_fc1 = weight_variable([(int(len(train_data[0])/config.pre_pool_size/config.pool1_size/config.pool2_size)*config.conv2_num_filters),config.hidden1_size])  
+  W_fc1 = weight_variable([(int(len(train_data[0])/config.pre_pool_size/config.pool1_size/config.pool2_size)*config.conv2_num_filters),config.hidden1_size])
   b_fc1 = bias_variable([config.hidden1_size])
 
   #I think the data should already be flat but this call is probably cheap
@@ -93,16 +93,16 @@ def main(_):
   #first hidden layer
   h1 = tf.nn.relu(tf.matmul(pool2_flat,W_fc1)+b_fc1)
 
-  W_fc2 = weight_variable([config.hidden1_size,len(train_labels[0])])  
+  W_fc2 = weight_variable([config.hidden1_size,len(train_labels[0])])
   b_fc2 = bias_variable([len(train_labels[0])])
- 
-  #these are the outputs 
+
+  #these are the outputs
   y_pre_weight = tf.matmul(h1,W_fc2)+b_fc2
-  
+
   #weight the outputs to inverse class frequency
-  y = tf.multiply(y_pre_weight,tf.cast(class_weights,tf.float32)) 
-  
-  #now calculate cross entropy on weighted inputs 
+  y = tf.multiply(y_pre_weight,tf.cast(class_weights,tf.float32))
+
+  #now calculate cross entropy on weighted inputs
   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y))
 
   train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
@@ -113,16 +113,20 @@ def main(_):
   for i in range(20000):
     #get a batch of 100 random training points from the training set
     batch_size = 100
-    batch_nums = np.random.choice(len(train_data_train[:,0]),batch_size) 
-    
-    #every 100th iteration let's calculate the train and validation accuracy     
+    batch_nums = np.random.choice(len(train_data_train[:,0]),batch_size)
+    sys.stdout.write("Batch {}".format(i))
+    sys.stdout.flush()
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+
+    #every 100th iteration let's calculate the train and validation accuracy
     if i%100 == 0:
       train_accuracy = accuracy.eval(feed_dict={
           x:train_data_train, y_: train_labels_train})
       validation_accuracy = accuracy.eval(feed_dict={
           x:train_data_validate, y_: train_labels_validate})
       print("Step {}, Training accuracy: {}, Validation accuracy: {}".format(i, train_accuracy,validation_accuracy))
-    
+
     #then train on the batch
     train_step.run(feed_dict={x: train_data_train[batch_nums], y_: train_labels_train[batch_nums]})
 
