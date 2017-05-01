c2eff4fbedc0b82ff59521f924351a7844d78eb9
Neal Jackson <nealjack@umich.edu>
2017-04-30 23:23:51 -0700

    generate_training now splits into seen and unseen sets
    Still need to properly concatenate the data, generate ordered labels,
    and create a weight vector for the classes. The model as is will
    accept numerical labels, so no need to one hot. We should model
    our dataset after the UCR data.

--- data_processing/generate_training.py
+++ data_processing/generate_training.py
@@ -12,6 +12,9 @@ import argparse
 from datetime import datetime, date
 import subprocess
 import glob
+import matplotlib.pyplot as plt
+
+SEC_IN_DAY = 24*60*60
 
 unseenTestRatio = .2
 
@@ -23,14 +26,21 @@ parser.add_argument('labelFile', metavar='O', type=str, nargs='+',
 
 args = parser.parse_args()
 
+def split_data(data):
+        splits = np.arange(SEC_IN_DAY, data.shape[0], SEC_IN_DAY)
+        days = np.array(np.split(data, splits))
+        return days
+
 
 #read the labels file and make a dict of lists
 labelFile = open(args.labelFile[0],'r')
 labels = labelFile.readline().strip().split(',')
 
 labelToFilenames = {}
+labelToData = {}
 for label in labels:
     labelToFilenames[label] = []
+    labelToData[label] = []
 
 #now get a list of data files
 dataFiles = os.listdir(args.inputdir[0])
@@ -48,42 +58,55 @@ for key in labelToFilenames:
     numFiles = len(labelToFilenames[key])
     print("{}: {} files".format(key,numFiles))
 
+print("\nLoading data and partitioning by day")
+for key in labelToFilenames:
+    print('working on ' + key)
+    for filename in labelToFilenames[key]:
+        data = split_data(np.load(args.inputdir[0] + '/' + filename))
+        #percentile = np.percentile(data[:,:,0], 90, 1)
+        #maximum = np.amax(data[:,:,0], 1)
+        #mean = np.mean(data[:,:,0], 1)
+        #keep = maximum > 3
+        labelToData[key].append(data)
 
 print('\nGenerate unseen set')
 
 labelToUnseen = {}
-labelToTest = {}
+labelToTrain = {}
 for key in labelToFilenames:
     numFiles = len(labelToFilenames[key])
-    if(numFiles <= 2):
+    if numFiles <= 2:
         print('No files for label ' + key + ', skipping...')
         continue
-    numPoints = 0
-
+    print('Attempting to partition ' + key)
     # load devices
-    devices = []
-    for filename in labelToFilenames[key]:
-        device = np.load(args.inputdir[0] + '/' + filename)
-        devices.append(device)
-        numPoints += device.shape[0]
+    numPoints = 0
+    devices = labelToData[key]
+    for data in devices:
+        numPoints += data.shape[0]*data.shape[1]
     numTries = 0
-    while numTries < 20:
+    while numTries < 1000:
         pick = np.random.choice(numFiles, int(np.ceil(numFiles * unseenTestRatio)), False)
-        unseenDevices = list(devices[i] for i in pick)
+        unseenDevices = [devices[i] for i in pick]
         unseenNumPoints = 0
         for device in unseenDevices:
-            unseenNumPoints += device.shape[0]
+            unseenNumPoints += device.shape[0] * device.shape[1]
+        print(key + 'pick represents {:.2g}'.format(unseenNumPoints/numPoints))
         if unseenNumPoints/numPoints > (unseenTestRatio - .1) and \
             unseenNumPoints/numPoints < (unseenTestRatio + .1):
             labelToUnseen[key] = unseenDevices
-            labelToTest[key] = devices
-            for ind in pick:
-                del labelToTest[key][ind]
+            labelToTrain[key] = [devices[i] for i in set(range(len(devices))) - set(pick)]
+
             break;
         numTries += 1
     if key not in labelToUnseen:
-        print('Failed to choose unseen device(s)')
-        exit()
-    #numTrain = int((1-unseenTestRatio)*numFiles)
-    #trainDict[key] = labelToFilenames[key][:numTrain]
-    #unseenTestDict[key] = labelToFilenames[key][numTrain:]
+        print('Failed to choose unseen device(s) for ' + key)
+        for i,device in enumerate(labelToData[key]):
+            print("device {} consists of {:.2}".format(i, device.shape[0]*device.shape[1]/numPoints))
+            print("Choices: ")
+            choices = [int(i) for i in input().split()]
+            labelToUnseen[key] = [devices[i] for i in choices]
+
+# Need to concatenate the two sets, preserving each set's labels
+# Need to split labelToTrain into training and validation
+# Also need to generate class weight vector for loss function
