ad7a322f45c2f8bfdc54c3a73c3ca2d5abaabf93
Neal Jackson <nealjack@umich.edu>
2017-05-01 21:51:03 -0700

    remove selected gpu + config

--- lstm/lstm_model.py
+++ lstm/lstm_model.py
@@ -74,68 +74,67 @@ class Model():
     self.input = tf.placeholder(tf.float32, [None, num_val], name = 'input')
     self.labels = tf.placeholder(tf.int64, [None], name='labels')
     self.keep_prob = tf.placeholder("float", name = 'Drop_out_keep_prob')
-    with tf.device('/gpu:3'):
-        print('Building Pooling layer')
-        with tf.name_scope("Pooling") as scope:
-            pool_input = tf.reshape(self.input,[-1,num_val,1,1], name = 'pool_input')
-            pre_pool = tf.nn.max_pool(pool_input, ksize=[1,config['pre_pool_size'],1,1], strides=[1,config['pre_pool_stride'],1,1], padding='SAME', name='pre_pool')
-            pre_pool = tf.squeeze(pre_pool, [2,3], name='pre_pool_reshape')
-        print('Building LSTM cells')
-        with tf.name_scope("LSTM_setup") as scope:
-          def single_cell():
-            return tf.contrib.rnn.DropoutWrapper(LSTMCell(hidden_size),output_keep_prob=self.keep_prob)
+    print('Building Pooling layer')
+    with tf.name_scope("Pooling") as scope:
+        pool_input = tf.reshape(self.input,[-1,num_val,1,1], name = 'pool_input')
+        pre_pool = tf.nn.max_pool(pool_input, ksize=[1,config['pre_pool_size'],1,1], strides=[1,config['pre_pool_stride'],1,1], padding='SAME', name='pre_pool')
+        pre_pool = tf.squeeze(pre_pool, [2,3], name='pre_pool_reshape')
+    print('Building LSTM cells')
+    with tf.name_scope("LSTM_setup") as scope:
+      def single_cell():
+        return tf.contrib.rnn.DropoutWrapper(LSTMCell(hidden_size),output_keep_prob=self.keep_prob)
 
-          cell = tf.contrib.rnn.MultiRNNCell([single_cell() for _ in range(num_layers)])
-          #initial_state = cell.zero_state(self.batch_size, tf.float32)
+      cell = tf.contrib.rnn.MultiRNNCell([single_cell() for _ in range(num_layers)])
+      #initial_state = cell.zero_state(self.batch_size, tf.float32)
 
-        input_list = tf.unstack(tf.expand_dims(pre_pool,axis=2),axis=1)
-        outputs,_ = core_rnn.static_rnn(cell, input_list, dtype=tf.float32)
+    input_list = tf.unstack(tf.expand_dims(pre_pool,axis=2),axis=1)
+    outputs,_ = core_rnn.static_rnn(cell, input_list, dtype=tf.float32)
 
-        output = outputs[-1]
+    output = outputs[-1]
 
-        print('Building Loss and Classification')
-        #Generate a classification from the last cell_output
-        #Note, this is where timeseries classification differs from sequence to sequence
-        #modelling. We only output to Softmax at last time step
-        with tf.name_scope("Softmax") as scope:
-          with tf.variable_scope("Softmax_params"):
-            softmax_w = tf.get_variable("softmax_w", [hidden_size, num_classes])
-            softmax_b = tf.get_variable("softmax_b", [num_classes])
-          class_weights = tf.constant(weight_vec)
-          logits = tf.multiply(tf.nn.xw_plus_b(output, softmax_w, softmax_b), class_weights)
-          #Use sparse Softmax because we have mutually exclusive classes
-          loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=self.labels,name = 'softmax')
-          self.cost = tf.reduce_sum(loss) / self.batch_size
-        with tf.name_scope("Evaluating_accuracy") as scope:
-          correct_prediction = tf.equal(tf.argmax(logits,1),self.labels)
-          self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-          h1 = tf.summary.scalar('accuracy',self.accuracy)
-          h2 = tf.summary.scalar('cost', self.cost)
+    print('Building Loss and Classification')
+    #Generate a classification from the last cell_output
+    #Note, this is where timeseries classification differs from sequence to sequence
+    #modelling. We only output to Softmax at last time step
+    with tf.name_scope("Softmax") as scope:
+      with tf.variable_scope("Softmax_params"):
+        softmax_w = tf.get_variable("softmax_w", [hidden_size, num_classes])
+        softmax_b = tf.get_variable("softmax_b", [num_classes])
+      class_weights = tf.constant(weight_vec)
+      logits = tf.multiply(tf.nn.xw_plus_b(output, softmax_w, softmax_b), tf.cast(class_weights, tf.float32))
+      #Use sparse Softmax because we have mutually exclusive classes
+      loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=self.labels,name = 'softmax')
+      self.cost = tf.reduce_sum(loss) / self.batch_size
+    with tf.name_scope("Evaluating_accuracy") as scope:
+      correct_prediction = tf.equal(tf.argmax(logits,1),self.labels)
+      self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+      h1 = tf.summary.scalar('accuracy',self.accuracy)
+      h2 = tf.summary.scalar('cost', self.cost)
 
 
-        print('Building Optimizer')
-        """Optimizer"""
-        with tf.name_scope("Optimizer") as scope:
-          tvars = tf.trainable_variables()
-          grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),max_grad_norm)   #We clip the gradients to prevent explosion
-          optimizer = tf.train.AdamOptimizer(learning_rate)
-          gradients = zip(grads, tvars)
-          self.train_op = optimizer.apply_gradients(gradients)
-          # Add histograms for variables, gradients and gradient norms.
-          # The for-loop loops over all entries of the gradient and plots
-          # a histogram. We cut of
-          #for gradient, variable in gradients:  #plot the gradient of each trainable variable
-          #       if isinstance(gradient, ops.IndexedSlices):
-          #         grad_values = gradient.values
-          #       else:
-          #         grad_values = gradient
+    print('Building Optimizer')
+    """Optimizer"""
+    with tf.name_scope("Optimizer") as scope:
+      tvars = tf.trainable_variables()
+      grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),max_grad_norm)   #We clip the gradients to prevent explosion
+      optimizer = tf.train.AdamOptimizer(learning_rate)
+      gradients = zip(grads, tvars)
+      self.train_op = optimizer.apply_gradients(gradients)
+      # Add histograms for variables, gradients and gradient norms.
+      # The for-loop loops over all entries of the gradient and plots
+      # a histogram. We cut of
+      #for gradient, variable in gradients:  #plot the gradient of each trainable variable
+      #       if isinstance(gradient, ops.IndexedSlices):
+      #         grad_values = gradient.values
+      #       else:
+      #         grad_values = gradient
 
-          #       tf.summary.histogram(variable.name, variable)
-          #       tf.summary.histogram(variable.name + "/gradients", grad_values)
-          #       tf.summary.histogram(variable.name + "/gradient_norm", clip_ops.global_norm([grad_values]))
+      #       tf.summary.histogram(variable.name, variable)
+      #       tf.summary.histogram(variable.name + "/gradients", grad_values)
+      #       tf.summary.histogram(variable.name + "/gradient_norm", clip_ops.global_norm([grad_values]))
 
-        #Final code for the TensorBoard
-        self.merged = tf.summary.merge_all()
-        self.init_op = tf.global_variables_initializer()
-        print('Finished computation graph')
+    #Final code for the TensorBoard
+    self.merged = tf.summary.merge_all()
+    self.init_op = tf.global_variables_initializer()
+    print('Finished computation graph')
 
