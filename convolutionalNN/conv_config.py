50da3a89749bdce00c1fe7c02f95b261b144e119
Josh Adkins <adkinsjd@umich.edu>
2017-05-02 22:05:45 -0700

    updated basic config file for test

--- convolutionalNN/conv_config.py
+++ convolutionalNN/conv_config.py
@@ -3,6 +3,11 @@ model_save_path = "./checkpoint1/model.ckpt"
 train_data = "../data/sets/train.npy"
 train_labels = "../data/sets/trainLabels.npy"
 train_ids = "../data/sets/trainID.npy"
+test_data = "../data/sets/unseen.npy"
+test_labels = "../data/sets/unseenLabels.npy"
+test_ids = "../data/sets/unseenID.npy"
+
+test = False
 
 #the size of the first max pooling layer
 pre_pool_size = 10
